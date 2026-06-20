82df664dbb467a64274c3ce76dd7052431c2d54e
rohitcoading111 <rp538220@gmail.com>
2026-06-20 15:40:45 +0530

    dropedown feature has been completed

---
+++ slack/script.js
@@ -0,0 +1,31 @@
+const featuresBtn = document.getElementById("featuresBtn");
+const solutionsBtn = document.getElementById("solutionsBtn");
+const resourcesBtn = document.getElementById("resourcesBtn");
+
+const featuresMenu = document.getElementById("featuresMenu");
+const solutionsMenu = document.getElementById("solutionsMenu");
+const resourcesMenu = document.getElementById("resourcesMenu");
+
+function closeAllMenus() {
+  featuresMenu.classList.remove("show");
+  solutionsMenu.classList.remove("show");
+  resourcesMenu.classList.remove("show");
+}
+
+featuresBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeAllMenus();
+  featuresMenu.classList.toggle("show");
+});
+
+solutionsBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeAllMenus();
+  solutionsMenu.classList.toggle("show");
+});
+
+resourcesBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeAllMenus();
+  resourcesMenu.classList.toggle("show");
+});
