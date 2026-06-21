5e342f23743210843c04740671f948971bb60e48
rohitcoading111 <rp538220@gmail.com>
2026-06-21 12:37:22 +0530

    making a book application

---
+++ book/script.js
@@ -0,0 +1,49 @@
+const bookTitleInput = document.querySelector("#book-title");
+const authorNameInput = document.querySelector("#author-name");
+const categorySelect = document.querySelector("#category");
+const filterSelect = document.querySelector("#filter");
+
+const addBookBtn = document.querySelector("#addbook-btn");
+const searchInput = document.querySelector("#search");
+
+const booksContainer = document.querySelector("#all-books");
+
+
+let books = [];
+console.log(books);
+
+
+addBookBtn.addEventListener("click",()=>{
+    let id = Date.now();
+    let bookTitle = bookTitleInput.value;
+    let authorName = authorNameInput.value;
+    let category = categorySelect.value;
+  if (bookTitleInput.value.trim() === "" || authorNameInput.value.trim() === "") {
+    return;
+}
+
+    let bookdata = {
+          id: id,         
+          title : bookTitle,
+          author: authorName,
+          category: category
+    }
+
+    books.push(bookdata);
+})
+
+ function renderBook(){
+   booksContainer.innerHTML = "" 
+   books.forEach((e)=>{
+      let bookDiv = document.createElement("div")
+      bookDiv.classList.add("book-card");
+      let h = document.createElement("h2")
+      let h3 = document.createElement("h3")
+      let p = document.createElement("p")
+      bookDiv.append(h,p,h3);
+      h.textContent = e.title;
+      p.textContent = e.author;
+      h3.textContent = e.category
+      booksContainer.append(bookDiv);
+   })
+}
